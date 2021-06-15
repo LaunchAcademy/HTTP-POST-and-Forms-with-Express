6eb87a6a49dfc5a4927d7f7c587731dc68587879
nwalberts <nwalberts5@gmail.com>
2021-06-15 13:24:59 -0400

    update for helpful comments

--- src/routes/animalsRouter.js
+++ src/routes/animalsRouter.js
@@ -5,36 +5,36 @@ import Animal from "../models/Animal.js"
 const animalsRouter = new express.Router()
 
 animalsRouter.get("/", (req, res) => {
-  console.log("rendering all animals!!")
 
   const allAnimals = Animal.findAll()
+
   res.render("index", { animals: allAnimals })
-  
   // res.render("index", { animals: Animal.findAll() })
 })
 
-// animalsRouter.get("/:id", (req,res) => {
-//   const id = req.params.id
-// })
 
 animalsRouter.post("/", (req, res) => {
-  const animalName = req.body.name
-  const animalType = req.body.type
-  
-  // debugger
-  
-  const animalObject = {name: animalName, type: animalType}
-  const newAnimal = new Animal(animalObject)
+  // Unpack the data 
+  const animalName = req.body.animalName
+  const animalType = req.body.animalType
+  // ES6 object desctructuring shortcut
+  // const { animalName , animalType } = animal 
 
-  // const newAnimal = new Animal({name: animalName, type: animalType})
+  // Optionally: validation 
+  if (animalName && animalType) {
 
-  // debugger
-  
-  newAnimal.save()
+    // Persist the new Animal to a database or file
+    const newAnimal = new Animal({ name: animalName, type: animalType })
+    newAnimal.save()
 
-  // debugger
-  
-  res.redirect("/animals")
+    // Redirect to see the result 
+    res.redirect("/animals")
+
+  } else {
+
+    // otherwise, re-render the page so that they can try again 
+    res.render("index", { animals: Animal.findAll() })
+  }
 })
 
 export default animalsRouter
