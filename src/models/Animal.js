4a5bbc20c279bc8d399e657b5d6b49964ca99fd1
Nick Alberts <nwalberts5@gmail.com>
2024-03-26 13:29:38 -0400

    44

--- src/models/Animal.js
+++ src/models/Animal.js
@@ -4,42 +4,37 @@ import _ from "lodash"
 const animalsPath = "animals.json"
 
 class Animal {
+  // constructor(animalDataObject) {
+  //   this.name = animalDataObject.animalName
+  //   this.type = animalDataObject.animalType
+  // }
   constructor({ name, type }) {
     this.name = name
     this.type = type
   }
 
   static findAll() {
-    const animalFileContents = fs.readFileSync(animalsPath)
-    const parsedContents = JSON.parse(animalFileContents)
-    const animalData = parsedContents.animals
-    // const animalData = JSON.parse(fs.readFileSync(animalsPath)).animals
-    // console.log("animals data", animalData)
+    const animalsJSON = fs.readFileSync(animalsPath)
+    const parsedAnimalData = JSON.parse(animalsJSON)
 
-    const animals = animalData.map((animal) => {
-      return new Animal(animal)
+    // const animals = []
+    // parsedAnimalData.animals.forEach(animalObject => {
+    //   animals.push(new Animal(animalObject))
+    // })
+
+    const animals = parsedAnimalData.animals.map(animalObject => {
+      return new Animal(animalObject)
     })
-    // console.log("array of Animal objects", animals)
+
     return animals
   }
 
   save() {
-    // "this" refers to instance of Animal that called on method `save` here
-    console.log("new animal to add", this)
-
-    // get all animals from JSON file
-    const allAnimals = this.constructor.findAll()
-    console.log("all animals", allAnimals)
-
-    // add our animal, `this` to the array
+    const allAnimals = Animal.findAll()
     allAnimals.push(this)
-    console.log("all animals after", allAnimals)
-
-    // stringify the data so it is back in JSON data type for file
-    // ensure data is formatted correctly, as object with key `animals`
-    console.log("stringified", JSON.stringify({ animals: allAnimals }))
-    // use fs to re-write file with the data
-    fs.writeFileSync(animalsPath, JSON.stringify({ animals: allAnimals }))
+    const animalsData = { animals: allAnimals }
+    const updatedAnimalsJSON = JSON.stringify(animalsData)
+    fs.writeFileSync(animalsPath, updatedAnimalsJSON)
   }
 }
 
--- src/routes/animalsRouter.js
+++ src/routes/animalsRouter.js
@@ -1,43 +1,25 @@
 import express from "express"
+import fs from "fs"
 
 import Animal from "../models/Animal.js"
 
 const animalsRouter = new express.Router()
 
 animalsRouter.get("/", (req, res) => {
-  const allAnimals = Animal.findAll()
+  const animals = Animal.findAll()
 
-  res.render("index", { animals: allAnimals, message: "first load! welcome!" })
-  // res.render("index", { animals: Animal.findAll() })
+  res.render("index", { animalsArray: animals })
 })
 
-animalsRouter.post("/", (req, res) => {
+animalsRouter.post("/banana", (req, res) => {
   // Unpack the data
   console.log("req body", req.body)
-  const animalName = req.body.animalName
-  const animalType = req.body.animalType
-  // ES6 object destructuring shortcut
-  // const { animalName , animalType } = animal
 
-  // Optionally: validation
-  if (animalName && animalType) {
-    // Persist the new Animal to a database or file
-    const newAnimal = new Animal({ type: animalType, name: animalName })
-    console.log("new animal", newAnimal)
-    newAnimal.save()
-
-    // "Happy path"
-    // Redirect to see the result
-    res.redirect("/animals")
-  } else {
-    // "Sad path"
-    console.log("sad path!!!")
-    // otherwise, re-render the page so that they can try again
-    res.render("index", {
-      animals: Animal.findAll(),
-      message: "need to supply both name and type!"
-    })
-  }
+  // create an animal object 
+  const newAnimal = new Animal({ name: req.body.animalName, type: req.body.animalType })
+  newAnimal.save()
+  // call its save method 
+  res.redirect("/animals")
 })
 
 export default animalsRouter
